803daa70b902203ec9292679af41aa7bde195f6b
Bingran Hu <bingran.hu@yscope.com>
2025-10-26 05:24:12 -0400

    Replace --trim with --id-only

--- src/log_archival_bench/scripts/docker_images/inspect.py
+++ src/log_archival_bench/scripts/docker_images/inspect.py
@@ -2,8 +2,6 @@
 """Inspect a Docker image for the specified benchmark engine and dump its JSON metadata."""
 
 import argparse
-import contextlib
-import json
 import subprocess
 import sys
 from pathlib import Path
@@ -31,32 +29,27 @@ def main(argv: list[str]) -> int:
         help="Path to dump the Docker image JSON metadata.",
     )
     args_parser.add_argument(
-        "--trim",
+        "--id-only",
         action="store_true",
-        help="""
-        Discard the 'Metadata.LastTagTime' entry in the output JSON that is unrelated to the image
-        digest.
-        """,
+        help="Only dump the Docker image ID which is a stable digest of the image content.",
     )
 
     parsed_args = args_parser.parse_args(argv[1:])
     engine_name = parsed_args.engine_name
     metadata_file = parsed_args.metadata_file
-    trim = parsed_args.trim
+    id_only = parsed_args.id_only
 
     validate_engine_name(engine_name)
 
     dump_cmds = ["docker", "inspect", "--type=image", get_image_name(engine_name)]
-    proc = subprocess.run(dump_cmds, capture_output=True, text=True, check=True)
-    inspect_json = json.loads(proc.stdout)
-    if trim:
-        with contextlib.suppress(IndexError, KeyError, TypeError):
-            inspect_json[0]["Metadata"].pop("LastTagTime")
+    if id_only:
+        dump_cmds.append("--format")
+        dump_cmds.append("{{.Id}}")
 
     metadata_file_path = Path(metadata_file)
     metadata_file_path.parent.mkdir(parents=True, exist_ok=True)
     with metadata_file_path.open("w", encoding="utf-8") as f:
-        json.dump(inspect_json, f, indent=4)
+        subprocess.run(dump_cmds, check=True, stdout=f)
 
     return 0
 
